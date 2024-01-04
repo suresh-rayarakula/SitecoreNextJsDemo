fd23af5a742ce5b3a1592bff5d4a27ee9fe9de45
suresh-rayarakula <69376751+suresh-rayarakula@users.noreply.github.com>
2024-01-05 01:06:05 +0530

    Update SearchResults.tsx

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -27,7 +27,7 @@ const SearchResults = () => {
 
   const handleSearch = async (searchKeyword: string): Promise<void> => {
     try {
-      const apiURL = 'https://discover.sitecorecloud.io/discover/v2/34207332';
+      const apiURL = 'https://discover.sitecorecloud.io/discover/v2/<search domain id>';
 
       const requestBody = {
         context: {
